13d12fbd130cbdef8c3a3d5b7159128c3c3a2a92
sivasankarchinnasamy <siva.vnit.1986@gmail.com>
2020-02-24 11:16:41 +0530

    first commit

---
+++ src/main/java/com/dp/jtp/DPTheory/strategy/Addition.java
@@ -0,0 +1,9 @@
+package com.dp.jtp.DPTheory.strategy;
+
+public class Addition implements Strategy{  	  
+    @Override  
+    public float calculation(float a, float b) {  
+        return a+b;  
+    }  
+  
+}
---
+++ src/main/java/com/dp/jtp/DPTheory/strategy/Strategy.java
@@ -0,0 +1,7 @@
+package com.dp.jtp.DPTheory.strategy;
+
+public interface Strategy {  
+    
+  public float calculation(float a, float b);  
+
+}  
